bc5d40656ff788a6a0549d0f09fc320b2c21994f
Denis Junuzovic <denisj1688@gmail.com>
2019-12-28 00:29:01 +0100

    regenerated loaders

--- scripts/initDataloaders.sh
+++ scripts/initDataloaders.sh
@@ -3,6 +3,5 @@
 cd loaders;
 rm ./*_gen.go
 go run github.com/vektah/dataloaden UserLoader string '*github.com/deni1688/bookingql/models.User';
-go run github.com/vektah/dataloaden CompanyLoader string '*github.com/deni1688/bookingql/models.Company';
 go run github.com/vektah/dataloaden VehicleLoader string '*github.com/deni1688/bookingql/models.Vehicle';
 go run github.com/vektah/dataloaden LocationLoader string '*github.com/deni1688/bookingql/models.Location';
